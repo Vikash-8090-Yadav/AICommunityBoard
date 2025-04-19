c177d0d9b0e53ea632b4df9b9f7c6aebb810ece4
Vikash Yadav <yadav621gzp@gmail.com>
2025-04-19 20:10:43 +0530

    Update network configuration for Rootstock Testnet

--- Frontend/config.jsx
+++ Frontend/config.jsx
@@ -1,1 +1,15 @@
 export const communityAddress = "0x2C8e10de89319BEdD4535fe668B659e6e1dF5593"
+
+export const networks = {
+  rootstockTestnet: {
+    chainId: `0x${Number(31).toString(16)}`,
+    chainName: "Rootstock Testnet",
+    nativeCurrency: {
+      name: "tRBTC",
+      symbol: "tRBTC",
+      decimals: 18
+    },
+    rpcUrls: ["https://public-node.testnet.rsk.co"],
+    blockExplorerUrls: ['https://explorer.testnet.rootstock.io/']
+  }
+};
