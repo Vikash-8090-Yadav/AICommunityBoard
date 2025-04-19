8c1d5f638690dc39d89d88737b8288f7d79cc1da
Vikash Yadav <yadav621gzp@gmail.com>
2025-04-19 20:10:46 +0530

    Update hardhat config for Rootstock Testnet

--- SmartContract/hardhat.config.js
+++ SmartContract/hardhat.config.js
@@ -11,7 +11,7 @@ task("accounts","Prints the list of the accounts",async (taskArgs , hre )=>{
 module.exports = {
   solidity: "0.8.10",
 
-  defaultNetwork: "edutestnet",
+  defaultNetwork: "rootstockTestnet",
   settings: {
     viaIR: true,
     optimizer: {
@@ -21,9 +21,9 @@ module.exports = {
   },
   networks:{
     hardhat:{},
-    edutestnet: {
-      url: "https://rpc.open-campus-codex.gelato.digital",
-      chainId: 656476,
+    rootstockTestnet: {
+      url: "https://public-node.testnet.rsk.co",
+      chainId: 31,
       accounts: ['5753e65f56865a161fbf41932a0d855139a4ce9dc20d82fb655bff393fc41702']
     },
   }
